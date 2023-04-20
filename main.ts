733b7518cb725e04aac8726b535c368b01060e40
Tsukitai <103410207+Tsukitai@users.noreply.github.com>
2023-04-20 09:02:54 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,50 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . . . .
+        . . # . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . . . .
+        . . # . .
+        . # # # .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . # . .
+        . . . . .
+        . . # . .
+        . # # # .
+        # . # . #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
+    basic.pause(100)
+})
